ed4c35714c76c42926a815c7e55b4f9ef9fb17fc
Tudor Buzu <buzu.tudor67@gmail.com>
2019-03-16 16:49:58 +0200

    added density_map_generation_shanghai_tech_part_B & updated save_dots_images

--- utils/data/annotations.py
+++ utils/data/annotations.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.io
 import skimage
 
 def load_bbs_from_txt(filename):
@@ -33,15 +34,25 @@ def bbs_list_to_dots_list(bbs_list):
         dots_list.append(dots)
     return dots_list
 
+def get_img_shapes(path, img_names):
+    img_shapes = []
+    for img_name in img_names:
+        img_shape = skimage.io.imread(f'{path}/{img_name}').shape
+        img_shapes.append(img_shape)
+    return img_shapes
+
 def create_dots_image(dots, img_shape):
     dots_img = np.zeros(img_shape, dtype=np.uint8)
     for (x, y) in dots:
         dots_img[y, x] = 255
     return dots_img
 
-def save_dots_images(dots_list, img_shape, img_names, save_path):
-    for dots, img_name in zip(dots_list, img_names):
-        dots_img = create_dots_image(dots, img_shape)
+def save_dots_images(dots_list, img_shapes, img_names, save_path):
+    if not isinstance(img_shapes, list):
+        img_shapes = [img_shapes] * len(img_names)
+        
+    for dots, img_shape, img_name in zip(dots_list, img_shapes, img_names):
+        dots_img = create_dots_image(dots, img_shape[:2])
         skimage.io.imsave(f'{save_path}/{img_name}', dots_img)
         
 def load_dots_images(gt_path, gt_dots_names):
@@ -51,3 +62,15 @@ def load_dots_images(gt_path, gt_dots_names):
         gt_dots_mask = skimage.io.imread(gt_dots_path) / 255.
         gt_dots.append(gt_dots_mask)
     return gt_dots
+
+def get_dots_from_mat_files(path, mat_names):
+    dots_list = []
+    for mat_name in mat_names:
+        dots = scipy.io.loadmat(f'{path}/{mat_name}')['image_info']
+        dots = np.floor(dots[0][0][0][0][0]).astype(np.int16)
+        dots_list.append(dots)
+    return dots_list
+
+def jpg_to_png(img_names):
+    img_names_png = ['.'.join(img_name.split('.')[:-1]) + '.png' for img_name in img_names]
+    return img_names_png
