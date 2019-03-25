a0949624a2d4e6fcbe0c72b9470f45f2a41008d2
Tudor Buzu <buzu.tudor67@gmail.com>
2019-03-26 00:18:46 +0200

    added experiments for vgg_cells, train_size=32, sigma=5, loss=mse, full img

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 ### experiments ###
 ARCHITECTURE_NAME = 'FCRN_A'
 DATASET_NAME = 'VGG_CELLS'
-SUB_EXPERIMENT_NAME = f'{DATASET_NAME.lower()}/test_n_64_sigma_5_randseed_645'
+SUB_EXPERIMENT_NAME = f'{DATASET_NAME.lower()}/n_32_sigma_5_randseed_325_loss_mse_full_img'
 DATASET_PATH = f'../../datasets/{DATASET_NAME.lower()}'
 TRAIN_PATH = f'{DATASET_PATH}/train'
 VAL_PATH = f'{DATASET_PATH}/val'
@@ -17,8 +17,8 @@ LOGS_PATH = f'./{SUB_EXPERIMENT_NAME}/logs/'
 LOGS_FILENAME = f'{LOGS_PATH}/logs.json'
 
 ### create validation split from initial train data ###
-VGG_CELLS_RAND_SEED = 645
-VGG_CELLS_N = 64 # num of train images for current experiments
+VGG_CELLS_RAND_SEED = 325
+VGG_CELLS_N = 32 # num of train images for current experiments
 VGG_CELLS_VAL_SIZE = 100 - VGG_CELLS_N
 CARPK_RAND_SEED = 128
 CARPK_VAL_SIZE = 128
