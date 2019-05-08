8931783d30ec62a4c13df1370d0b633138d29afe
Tudor Buzu <buzu.tudor67@gmail.com>
2019-05-08 21:01:49 +0300

    added 2_unet exp results on carpk (logcosh loss, patches 32*(128x128), epochs=15, batch_size=32)

--- experiments/2_unet/config.py
+++ experiments/2_unet/config.py
@@ -1,7 +1,7 @@
 ### experiments ###
 ARCHITECTURE_NAME = 'UNet'
 DATASET_NAME = 'carpk'
-SUB_EXPERIMENT_NAME = f'{DATASET_NAME.lower()}/sigma_10_loss_mse_patch_32_128x128_epochs_5'
+SUB_EXPERIMENT_NAME = f'{DATASET_NAME.lower()}/sigma_10_loss_logcosh_patch_32_128x128_epochs_15'
 DATASET_PATH = f'../../datasets/{DATASET_NAME.lower()}'
 TRAIN_PATH = f'{DATASET_PATH}/train'
 VAL_PATH = f'{DATASET_PATH}/val'
@@ -37,7 +37,7 @@ elif DATASET_NAME.lower() == 'shanghai_tech/part_b':
     VAL_SIZE = SHANGHAI_TECH_PART_B_VAL_SIZE
 
 ### train ###
-LOSS_NAME = 'mean_squared_error'
+LOSS_NAME = 'logcosh'
 
 IMG_DIM = None
 if DATASET_NAME.lower() == 'vgg_cells':
@@ -50,7 +50,7 @@ elif DATASET_NAME.lower() == 'shanghai_tech/part_b':
 PATCH_DIM = (128, 128, 3)
 PATCHES_PER_IMAGE = 32
 BATCH_SIZE = 32
-EPOCHS = 5
+EPOCHS = 15
 SHUFFLE = True
 
 ### ground truth ###
