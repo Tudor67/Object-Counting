1244f2fa8c2ef03b1040ce84d26b87481a581d91
Tudor Buzu <buzu.tudor67@gmail.com>
2019-07-30 17:07:26 +0300

    added 4_unet_data_aug res on vgg_cells (mse loss, patches 4*(128x128), batch_size=16, epochs=30)

--- experiments/4_unet_data_aug/config.py
+++ experiments/4_unet_data_aug/config.py
@@ -1,7 +1,7 @@
 ### experiments ###
 ARCHITECTURE_NAME = 'UNet'
 DATASET_NAME = 'vgg_cells'
-SUB_EXPERIMENT_NAME = f'{DATASET_NAME.lower()}/n_64_sigma_5_randseed_645_loss_logcosh_full_img'
+SUB_EXPERIMENT_NAME = f'{DATASET_NAME.lower()}/n_64_sigma_5_randseed_645_loss_mse_patch_4_128x128'
 DATASET_PATH = f'../../datasets/{DATASET_NAME.lower()}'
 TRAIN_PATH = f'{DATASET_PATH}/train'
 VAL_PATH = f'{DATASET_PATH}/val'
@@ -36,7 +36,7 @@ elif DATASET_NAME.lower() == 'shanghai_tech/part_b':
     VAL_SIZE = SHANGHAI_TECH_PART_B_VAL_SIZE
 
 ### train ###
-LOSS_NAME = 'logcosh'
+LOSS_NAME = 'mean_squared_error'
 LEARNING_RATE = 1e-3
 
 IMG_DIM = None
@@ -47,8 +47,8 @@ elif DATASET_NAME.lower() == 'carpk':
 elif DATASET_NAME.lower() == 'shanghai_tech/part_b':
     IMG_DIM = (768, 1024, 3) # ShanghaiTech
         
-PATCH_DIM = IMG_DIM
-PATCHES_PER_IMAGE = 1
+PATCH_DIM = (128, 128, 3)
+PATCHES_PER_IMAGE = 4
 BATCH_SIZE = 16
 EPOCHS = 30
 SHUFFLE = True
